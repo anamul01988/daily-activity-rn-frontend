ddf00739cbd0acdcaf3ae51a3de06c287df65941
anamul01988 <anamulhaque0198877@gmail.com>
2024-03-30 23:52:07 +0600

    project is initialized & absolute import issue is solved

---
+++ src/components/button.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const Button = () => {
+  return (
+    <View>
+      <Text> Button Component ddfddf</Text>
+    </View>
+  );
+};
+export default Button;
+const styles = StyleSheet.create({});
